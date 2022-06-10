9d0f97445d456098bc1d5fe731785b7e87dd37b9
lojy4231 <lojy4231@gmail.com>
2022-06-10 21:04:09 +0900

    회원가입, 로그인 라우터, 모델 생성

--- src/models/user.js
+++ src/models/user.js
@@ -1,33 +1,33 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const saltRounds = Number(process.env.SALT_KEY);
 
 const UserSchema = new mongoose.Schema({
     useremail: String,
-  nickname: String,
-  password: String,
+    nickname: String,
+    password: String,
 });
 UserSchema.virtual("userId").get(function () {
-  return this._id.toHexString();
+    return this._id.toHexString();
 });
 UserSchema.set("toJSON", {
-  virtuals: true,
+    virtuals: true,
 });
 UserSchema.pre("save", function (next) {
-  const user = this;
+    const user = this;
 
-  if (user.isModified("password")) {
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-      if (err) return next(err);
-      bcrypt.hash(user.password, salt, function (err, hashPassword) {
-        if (err) return next(err);
-        user.password = hashPassword;
+    if (user.isModified("password")) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function (err, hashPassword) {
+                if (err) return next(err);
+                user.password = hashPassword;
+                next();
+            });
+        });
+    } else {
         next();
-      });
-    });
-  } else {
-    next();
-  }
+    }
 });
 
 module.exports = mongoose.model("User", UserSchema);
