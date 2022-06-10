73a6ab4da0fab065e91def0e5e861e686e7215e3
lojy4231 <lojy4231@gmail.com>
2022-06-10 23:56:54 +0900

    미들웨어 수정

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const Users = require("../models/")
+const {Users} = require("../models/");
 
 module.exports = (req, res, next) => {
     try {
@@ -14,8 +14,9 @@ module.exports = (req, res, next) => {
             });
             return;
         }
-
+        console.log(process.env.NODE_JWT);
         const { nickname } = jwt.verify(tokenValue, process.env.NODE_JWT)
+        console.log({nickname});
         
         if(!nickname){
             res.status(401).send({
@@ -24,7 +25,8 @@ module.exports = (req, res, next) => {
             });
             return;
         }
-        Users.findById(nickname).exec().then((user) => {
+        console.log(1)
+        Users.findOne({ nickname }).exec().then((user) => {
             res.locals.user = user;
             next();
         });
@@ -32,7 +34,7 @@ module.exports = (req, res, next) => {
 
     } catch (error) {
         res.status(401).send({
-            errorMessage: "로그인 후 이용해주세요"
+            errorMessage: error.message
 
         });
         return;
